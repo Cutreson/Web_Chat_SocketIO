79fa0cdbc079609896f78e6d5455583755b5adad
Cutreson <sondh@svrqo.com>
2022-02-25 14:14:34 +0700

    web socketIO

--- public/xuly.js
+++ public/xuly.js
@@ -1,4 +1,43 @@
+var socket = io("http://localhost:3000")
+
+socket.on("server-send-register-error", function(){
+    alert("User name da ton tai...");
+});
+
+socket.on("server-send-register-ok", function(data){
+    $("#currentUser").html(data);
+    $("#loginForm").hide(500);
+    $("#chatForm").show(1000);
+});
+
+socket.on("server-send-listUser", function(data){
+    $("#boxContent").html("");
+    data.forEach(function(i){
+        $("#boxContent").append("<div class = 'userOnline'>" + i + "</div>");
+    });
+});
+
+socket.on("server-send-Message", function(data){
+    $("#listMessages").append("<div class = 'message'>" + data.un + ": " + data.nd + "</div>");
+});
+
 $(document).ready(function() {
     $("#loginForm").show();
     $("#chatForm").hide();
+
+    $("#btnRegister").click(function(){
+        socket.emit("client-send-Username", $("#txtUsername").val());
+    })
+
+    $("#btnLogout").click(function(){
+        socket.emit("logout");
+        $("#loginForm").show(1000);
+        $("#chatForm").hide(500);
+    })
+
+    $("#btnMessage").click(function(){
+        socket.emit("user-send-Message", $("#txtMessage").val());
+        document.getElementById('txtMessage').value = "";
+    })
+
 });
--- server.js
+++ server.js
@@ -9,6 +9,36 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 server.listen(3000);
 
+var listUser = [];
+
+io.on("connection", function(socket){
+    console.log(socket.id + " da ket noi...");
+
+    socket.on("client-send-Username", function(data){
+        if(listUser.indexOf(data) >=0){
+            //dang ki that bai
+            socket.emit("server-send-register-error");
+        }
+        else{
+            //dang ki thanh cong
+            listUser.push(data);
+            socket.Username = data;
+            socket.emit("server-send-register-ok", data);
+            io.sockets.emit("server-send-listUser",listUser);
+        }
+    });
+
+    socket.on("logout", function(){
+        listUser.splice(listUser.indexOf(socket.Username), 1);
+        socket.broadcast.emit("server-send-listUser",listUser);
+    });
+
+    socket.on("user-send-Message", function(data){
+        console.log(data);
+        io.sockets.emit("server-send-Message", {un:socket.Username, nd:data});
+    });
+});
+
 app.get("/", function(req, res){
     res.render("home");
 });
